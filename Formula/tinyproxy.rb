deliretzua/homebrew-core

commit 6ddcc5ef17d9a552a8ab52a135fcec6352e8d435

Fix 'inreplace ... do' single-substitution errors (#4022)

Formula/tinyproxy.rb
@@ -43,9 +43,8 @@ class Tinyproxy < Formula
 
     # Fix broken XML lint
     # See: https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=154624
-    inreplace ["docs/man5/Makefile", "docs/man8/Makefile"] do |s|
-      s.gsub! "-f manpage", "-f manpage \\\n  -L"
-    end
+    inreplace %w[docs/man5/Makefile docs/man8/Makefile"], "-f manpage",
+                                                          "-f manpage \\\n  -L"
 
     system "make", "install"
   end

Formula/automysqlbackup.rb
@@ -12,9 +12,7 @@ class Automysqlbackup < Formula
       s.gsub! "/etc", etc
       s.gsub! "/var", var
     end
-    inreplace "automysqlbackup.conf" do |s|
-      s.gsub! "/var", var
-    end
+    inreplace "automysqlbackup.conf", "/var", var
 
     conf_path = (etc/"automysqlbackup")
     conf_path.install "automysqlbackup.conf" unless (conf_path/"automysqlbackup.conf").exist?

Formula/jdnssec-tools.rb
@@ -17,9 +17,7 @@ class JdnssecTools < Formula
   depends_on :java
 
   def install
-    inreplace Dir["bin/*"] do |s|
-      s.gsub! /basedir=.*/, "basedir=#{libexec}"
-    end
+    inreplace Dir["bin/*"], /basedir=.*/, "basedir=#{libexec}"
     bin.install Dir["bin/*"]
     (libexec/"lib").install Dir["lib/*"]
   end

Formula/logrotate.rb
@@ -31,9 +31,7 @@ class Logrotate < Formula
     man5.install "logrotate.conf.5"
 
     mv "examples/logrotate-default", "logrotate.conf"
-    inreplace "logrotate.conf" do |s|
-      s.gsub! "/etc/logrotate.d", "#{etc}/logrotate.d"
-    end
+    inreplace "logrotate.conf", "/etc/logrotate.d", "#{etc}/logrotate.d"
 
     etc.install "logrotate.conf"
     (etc/"logrotate.d").mkpath

Formula/mitie.rb
@@ -28,9 +28,7 @@ class Mitie < Formula
       (share/"MITIE-models").install resource("models-english")
     end
 
-    inreplace "mitielib/makefile" do |s|
-      s.gsub!(/libmitie.so/, "libmitie.dylib")
-    end
+    inreplace "mitielib/makefile", "libmitie.so", "libmitie.dylib"
     system "make", "mitielib"
     system "make"
 

Formula/nrpe.rb
@@ -37,9 +37,7 @@ class Nrpe < Formula
                           "--enable-ssl",
                           "--enable-command-args"
 
-    inreplace "src/Makefile" do |s|
-      s.gsub! "$(LIBEXECDIR)", "$(SBINDIR)"
-    end
+    inreplace "src/Makefile", "$(LIBEXECDIR)", "$(SBINDIR)"
 
     system "make", "all"
     system "make", "install"

Formula/ranger.rb
@@ -17,9 +17,11 @@ class Ranger < Formula
   depends_on :python if MacOS.version <= :leopard
 
   def install
-    inreplace %w[ranger.py ranger/ext/rifle.py] do |s|
-      s.gsub! "#!/usr/bin/python", "#!#{PythonRequirement.new.which_python}"
-    end if MacOS.version <= :leopard
+    if MacOS.version <= :leopard
+      inreplace %w[ranger.py ranger/ext/rifle.py],
+        "#!/usr/bin/python", "#!#{PythonRequirement.new.which_python}"
+    end
+
 
     man1.install "doc/ranger.1"
     libexec.install "ranger.py", "ranger"

Formula/sleepwatcher.rb
@@ -34,13 +34,10 @@ class Sleepwatcher < Formula
     (prefix + "etc/sleepwatcher").install Dir["config/rc.*"]
 
     # Write the launchd scripts
-    inreplace Dir["config/*.plist"] do |s|
-      s.gsub! "/usr/local/sbin", HOMEBREW_PREFIX/"sbin"
-    end
+    inreplace Dir["config/*.plist"], "/usr/local/sbin", HOMEBREW_PREFIX/"sbin"
 
-    inreplace "config/de.bernhard-baehr.sleepwatcher-20compatibility.plist" do |s|
-      s.gsub! "/etc", (etc + "sleepwatcher")
-    end
+    inreplace "config/de.bernhard-baehr.sleepwatcher-20compatibility.plist",
+      "/etc", etc/"sleepwatcher"
 
     prefix.install Dir["config/*.plist"]
   end

commit 08b892abdbbb1fc47b3f907d2bb2f8d00d2490fa

tinyproxy: fix inreplace paths (#4624)
A find/replace on `inreplace` usage in https://github.com/Homebrew/homebrew-core/commit/6ddcc5ef17d9a552a8ab52a135fcec6352e8d435 broke the tinyproxy formula by leaving a quote in a filename when it should've been stripped.

Formula/tinyproxy.rb
@@ -43,8 +43,8 @@ class Tinyproxy < Formula
 
     # Fix broken XML lint
     # See: https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=154624
-    inreplace %w[docs/man5/Makefile docs/man8/Makefile"], "-f manpage",
-                                                          "-f manpage \\\n  -L"
+    inreplace %w[docs/man5/Makefile docs/man8/Makefile], "-f manpage",
+                                                         "-f manpage \\\n  -L"
 
     system "make", "install"
   end