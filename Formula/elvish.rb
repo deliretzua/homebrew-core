deliretzua/homebrew-core

commit 6f23fd2fc85af409c54516caa0cd8ec59da1c05c

elvish: fix install with vendored golang library
Signed-off-by: Shengjing Zhu <i@zhsj.me>

Closes #16315.

Signed-off-by: FX Coudert <fxcoudert@gmail.com>

Formula/elvish.rb
@@ -16,9 +16,10 @@ class Elvish < Formula
 
   def install
     ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/elves").mkpath
-    ln_sf buildpath, buildpath/"src/github.com/elves/elvish"
-    system "go", "build", "-o", bin/"elvish"
+    (buildpath/"src/github.com/elves/elvish").install buildpath.children
+    cd "src/github.com/elves/elvish" do
+      system "go", "build", "-o", bin/"elvish"
+    end
   end
 
   test do

commit 78296464ec20e93e66871869ff625d3dd4411a0b

elvish: fix metafiles regression from 6f23fd2fc85af
Closes #16345.

Signed-off-by: FX Coudert <fxcoudert@gmail.com>

Formula/elvish.rb
@@ -20,6 +20,7 @@ class Elvish < Formula
     (buildpath/"src/github.com/elves/elvish").install buildpath.children
     cd "src/github.com/elves/elvish" do
       system "go", "build", "-o", bin/"elvish"
+      prefix.install_metafiles
     end
   end
 