deliretzua/homebrew-core

commit 2d6e8952edae1ea029696bad8de10a7c23b71e29

r: fix brew style.
Part of https://github.com/Homebrew/brew/pull/11955.

Formula/r.rb
@@ -59,13 +59,11 @@ class R < Formula
       "--with-cairo",
     ]
 
-    on_macos do
+    if OS.mac?
       args << "--without-x"
       args << "--with-aqua"
-    end
-
-    on_linux do
-      args << "--libdir=#{lib}" # avoid using lib64 on CentOS
+    else
+      c args << "--libdir=#{lib}" # avoid using lib64 on CentOS
 
       # Avoid references to homebrew shims
       args << "LD=ld"
@@ -110,10 +108,7 @@ class R < Formula
     lib.install_symlink Dir[r_home/"lib/*"]
 
     # avoid triggering mandatory rebuilds of r when gcc is upgraded
-    check_replace = true
-    on_linux do
-      check_replace = false
-    end
+    check_replace = OS.mac?
     inreplace lib/"R/etc/Makeconf", Formula["gcc"].prefix.realpath,
                                     Formula["gcc"].opt_prefix,
                                     check_replace

commit 07330fe3cad92654f4d9ed827c78ca0dcddc8b68

r: fix typo
Accidentally introduced in 2d6e8952edae1ea029696bad8de10a7c23b71e29.

Closes #85770.

Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

Formula/r.rb
@@ -63,7 +63,7 @@ class R < Formula
       args << "--without-x"
       args << "--with-aqua"
     else
-      c args << "--libdir=#{lib}" # avoid using lib64 on CentOS
+      args << "--libdir=#{lib}" # avoid using lib64 on CentOS
 
       # Avoid references to homebrew shims
       args << "LD=ld"