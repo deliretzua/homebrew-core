deliretzua/homebrew-core

commit fcc08e625a11c1c3a811779bf38fb5a4cbd90028

vcpkg 2022.06.17
* vcpkg 2022.06.17
* update test

Closes #104979.

Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

Formula/vcpkg.rb
@@ -1,9 +1,9 @@
 class Vcpkg < Formula
   desc "C++ Library Manager"
   homepage "https://github.com/microsoft/vcpkg"
-  url "https://github.com/microsoft/vcpkg-tool/archive/2022-05-05.tar.gz"
-  version "2022.05.05"
-  sha256 "c2d02a979b648d8e640c1704d72766e68ab783f03c6eb89f1ad5a6645fd7f547"
+  url "https://github.com/microsoft/vcpkg-tool/archive/2022-06-17.tar.gz"
+  version "2022.06.17"
+  sha256 "4b1f477510cde965f230317305b0b111bce20f21e02c1c0e73d7bb6ebec0e783"
   license "MIT"
   head "https://github.com/microsoft/vcpkg-tool.git", branch: "main"
 
@@ -59,7 +59,7 @@ class Vcpkg < Formula
   end
 
   test do
-    message = "Error: Could not detect vcpkg-root. You must define the VCPKG_ROOT environment variable"
+    message = "error: Could not detect vcpkg-root."
     assert_match message, shell_output("#{bin}/vcpkg search sqlite", 1)
   end
 end

commit 9bc5159dc443b9d94486eabef51789193adefad1

vcpkg: fix test
Let's return the test assertion from b0436ef22ef6dee52419f18881291dbe5ead09d5
to check for a broken `inreplace`.

The test was then broken by fcc08e625a11c1c3a811779bf38fb5a4cbd90028, so
let's leave a comment documenting that the `inreplace` also needs fixing
whenever the test does.

Closes #106201.

Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

Formula/vcpkg.rb
@@ -59,7 +59,8 @@ class Vcpkg < Formula
   end
 
   test do
-    message = "error: Could not detect vcpkg-root."
+    # DO NOT CHANGE. If the test breaks then the `inreplace` needs fixing.
+    message = "error: Could not detect vcpkg-root. You must define the VCPKG_ROOT environment variable"
     assert_match message, shell_output("#{bin}/vcpkg search sqlite", 1)
   end
 end