deliretzua/homebrew-core

commit 4f8e7f9f06e58aeeca29a6b8680cd72410270c2c

icu4c 71.1
Co-authored-by: Bo Anderson <mail@boanderson.me>

Formula/icu4c.rb
@@ -1,9 +1,9 @@
 class Icu4c < Formula
   desc "C/C++ and Java libraries for Unicode and globalization"
-  homepage "https://icu.unicode.org/home"
-  url "https://github.com/unicode-org/icu/releases/download/release-70-1/icu4c-70_1-src.tgz"
-  version "70.1"
-  sha256 "8d205428c17bf13bb535300669ed28b338a157b1c01ae66d31d0d3e2d47c3fd5"
+  homepage "https://site.icu-project.org/home"
+  url "https://github.com/unicode-org/icu/releases/download/release-71-1/icu4c-71_1-src.tgz"
+  version "71.1"
+  sha256 "67a7e6e51f61faf1306b6935333e13b2c48abd8da6d2f46ce6adca24b1e21ebf"
   license "ICU"
 
   livecheck do

commit cbd0903b5b521de545132b0eddfb03359b768e25

icu4c: update homepage url
It looks like commit 4f8e7f9 inadvertently reversed c9027bc.
This commit restores the homepage url from the previous commit.

Closes #110465.

Signed-off-by: FX Coudert <fxcoudert@gmail.com>
Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

Formula/icu4c.rb
@@ -1,6 +1,6 @@
 class Icu4c < Formula
   desc "C/C++ and Java libraries for Unicode and globalization"
-  homepage "https://site.icu-project.org/home"
+  homepage "https://icu.unicode.org/home"
   url "https://github.com/unicode-org/icu/releases/download/release-71-1/icu4c-71_1-src.tgz"
   version "71.1"
   sha256 "67a7e6e51f61faf1306b6935333e13b2c48abd8da6d2f46ce6adca24b1e21ebf"